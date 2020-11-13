618322cd4f452bb5267fcc59530be15de59fe017
KindredSoul <jabriltb2@gmail.com>
2020-11-13 12:42:14 -0600

    Initial Commit

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { axiosWithAuth } from "./utils/axiosWithAuth";
 
 const initialColor = {
   color: "",
@@ -21,17 +22,37 @@ const ColorList = ({ colors, updateColors }) => {
     // Make a put request to save your updated color
     // think about where will you get the id from...
     // where is is saved right now?
+    axiosWithAuth()
+      .put(`/colors/${colorToEdit.id}`, colorToEdit)
+      .then(res => {
+        console.log(res.data)
+        updateColors(
+          colors.map(color=>color.id === colorToEdit.id ? color=colorToEdit : color)
+        )
+        setEditing(false)
+      })
   };
 
   const deleteColor = color => {
     // make a delete request to delete this color
+    axiosWithAuth()
+      .delete(`/colors/${color.id}`)
+      .then(res => {
+        console.log(res.data)
+        window.location.reload()
+      })
+      .catch(err => console.dir(err))
   };
+
+  if(!colors){
+    return(<h1>Loading Colors...</h1>)
+  }
 
   return (
     <div className="colors-wrap">
       <p>colors</p>
       <ul>
-        {colors.map(color => (
+        {colors?.map(color => (
           <li key={color.color} onClick={() => editColor(color)}>
             <span>
               <span className="delete" onClick={e => {
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,44 @@
-import React from "react";
+import Axios from "axios";
+import React, { useState } from "react";
+import { useHistory } from 'react-router-dom';
+
+const initialState = {
+    username: "",
+    password: "",
+}
 
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  const [form, setForm] = useState(initialState)
+    const history = useHistory()
+
+    const login = (e) => {
+        e.preventDefault()
+        console.log(form)
+        Axios.post("http://localhost:5000/api/login", form)
+            .then(res=> {
+                console.log(res.data)
+                localStorage.setItem('token', res.data.payload)
+                history.push('/bubble-hub')
+            })
+            .catch(err=>console.dir(err))
+    }
+
+    const handleChange = e => {
+        setForm({...form, [e.target.name]:e.target.value})
+    }
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <div>
+        <form onSubmit={(e)=>login(e)} >
+          <input type="text" name="username" value={form.username} onChange={handleChange} />
+          <input type="password" name="password" value={form.password} onChange={handleChange} />
+          <input type="submit" value="Login" />
+        </form>
+      </div>
+
     </>
   );
 };
